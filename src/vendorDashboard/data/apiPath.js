3e81bdd2871e8141ae0890514396167fc813b191
VishnuPriyaAthukuri1109 <vishnupriyaatukuri@example.com>
2025-07-02 19:26:32 +0530

    final

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 // Server URL
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
 // Deployment URL
-// export const API_URL = "https://backend-nodejs-swiggy-rxpx.onrender.com/";
+export const API_URL = "https://backend-nodejs-swiggy-rxpx.onrender.com/";
